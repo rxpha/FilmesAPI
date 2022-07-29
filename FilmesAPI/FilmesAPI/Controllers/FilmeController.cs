ea1241f0ee0964c350fd1e142cb503148dbfc36b
rxpha <44279227+rxpha@users.noreply.github.com>
2022-07-29 16:52:03 -0400

    Add files via upload
    version: 0.1.0

---
+++ FilmesAPI/FilmesAPI/Controllers/FilmeController.cs
@@ -0,0 +1,30 @@
+﻿using FilmesAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FilmesAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class FilmeController : ControllerBase
+    {
+        private static List<Filme> filmes = new List<Filme>();
+
+        [HttpPost]
+        public void AdicionaFilme([FromBody]Filme filme)
+        {
+            filmes.Add(filme);
+            Console.WriteLine(filme.Titulo + " is working!!");
+        }
+
+        [HttpGet]
+        public List<Filme> RecuperaFilmes()
+        {
+            return filmes;
+        }
+    }
+}
---
+++ FilmesAPI/FilmesAPI/Models/Filme.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FilmesAPI.Models
+{
+    public class Filme
+    {
+        [Required (ErrorMessage = "O campo Título é obrigatório!")]
+        public string Titulo { get; set; }
+        [Required (ErrorMessage = "O campo Diretor é obrigatório!")]
+        public string Diretor { get; set; }
+        [StringLength(10, ErrorMessage = "O campo gênero não pode ter mais de 10 caracteres!")]
+        public string Genero { get; set; }
+        [Required, Range(1, 600, ErrorMessage = "A duração só pode estar entre 1 e 600 minutos!")]
+        public int Duracao { get; set; }
+    }
+}
