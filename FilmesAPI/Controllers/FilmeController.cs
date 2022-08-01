fe43e06d3739c5df94b7895bd9ea1da524cf1d37
rxpha <44279227+rxpha@users.noreply.github.com>
2022-08-01 09:36:23 -0400

    add files via upload
    some changes were added.

---
+++ FilmesAPI/Controllers/FilmeController.cs
@@ -0,0 +1,39 @@
+﻿using FilmesAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FilmesAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class FilmeController : ControllerBase
+    {
+        private static List<Filme> filmes = new List<Filme>();
+        private static int id = 1;
+
+        [HttpPost]
+        public IActionResult AdicionaFilme([FromBody] Filme filme)
+        {
+            filme.Id = id++;
+            filmes.Add(filme);
+            return CreatedAtAction(nameof(RecuperaFilmePorID), new { Id = filme.Id }, filme);
+        }
+
+        [HttpGet]
+        public IActionResult RecuperaFilmes()
+        {
+            return Ok(filmes);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult RecuperaFilmePorID(int id)
+        {
+            Filme filme = filmes.FirstOrDefault(filme => filme.Id == id);
+            return filme != null ? Ok(filme) : NotFound();
+        }
+    }
+}
